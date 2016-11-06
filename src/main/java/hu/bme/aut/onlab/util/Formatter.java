0faaea7a7220cd2ea0902819c94d1812994c8190
Logan <pyrolanddev@gmail.com>
2016-11-06 19:27:06 +0100

    - Added null parameter handling.

--- src/main/java/hu/bme/aut/onlab/util/Formatter.java
+++ src/main/java/hu/bme/aut/onlab/util/Formatter.java
@@ -25,17 +25,22 @@ public class Formatter {
 	 * @return
 	 */
 	public static String formatTimeStamp(Timestamp timestamp) {
-		LocalDateTime localDateTime = timestamp.toLocalDateTime();
-		int year = localDateTime.getYear();
-		int dayOfYear = localDateTime.getDayOfYear();
-		LocalDateTime now = LocalDateTime.now();
-		if (year == now.getYear() && dayOfYear == now.getDayOfYear()) {
-			return TODAY + localDateTime.format(timeFormatter);
-		} else if (year == now.getYear() && dayOfYear == now.getDayOfYear()-1) {
-			return YESTERDAY + localDateTime.format(timeFormatter);
-		} else {
-			return localDateTime.format(fullDateFormatter);
-		} 
+		if (timestamp != null) {
+			LocalDateTime localDateTime = timestamp.toLocalDateTime();
+			int year = localDateTime.getYear();
+			int dayOfYear = localDateTime.getDayOfYear();
+			LocalDateTime now = LocalDateTime.now();
+			if (year == now.getYear() && dayOfYear == now.getDayOfYear()) {
+				return TODAY + localDateTime.format(timeFormatter);
+			} else if (year == now.getYear() && dayOfYear == now.getDayOfYear() - 1) {
+				return YESTERDAY + localDateTime.format(timeFormatter);
+			} else {
+				return localDateTime.format(fullDateFormatter);
+			}
+		}
+		else {
+			return "";
+		}
 	}
 
 	/**
@@ -48,30 +53,35 @@ public class Formatter {
 	 * @return
 	 */
 	public static String formatTimeStampForMessage(Timestamp timestamp) {
-		LocalDateTime localDateTime = timestamp.toLocalDateTime();
-		int year = localDateTime.getYear();
-		int dayOfYear = localDateTime.getDayOfYear();
-		int hour = localDateTime.getHour();
-		int minute = localDateTime.getMinute();
-		LocalDateTime now = LocalDateTime.now();
-		if (year == now.getYear() && dayOfYear == now.getDayOfYear()) {
-			int minutesOfTimeStamp = hour*60+minute;
-			int minutesAnHourAgo = now.getHour()*60+now.getMinute()-60;
-			if (minutesOfTimeStamp > minutesAnHourAgo) {
-				int m = minutesOfTimeStamp - minutesAnHourAgo;
-				if (m == 1) {
-					return "1 minutes ago";
+		if (timestamp != null) {
+			LocalDateTime localDateTime = timestamp.toLocalDateTime();
+			int year = localDateTime.getYear();
+			int dayOfYear = localDateTime.getDayOfYear();
+			int hour = localDateTime.getHour();
+			int minute = localDateTime.getMinute();
+			LocalDateTime now = LocalDateTime.now();
+			if (year == now.getYear() && dayOfYear == now.getDayOfYear()) {
+				int minutesOfTimeStamp = hour * 60 + minute;
+				int minutesAnHourAgo = now.getHour() * 60 + now.getMinute() - 60;
+				if (minutesOfTimeStamp > minutesAnHourAgo) {
+					int m = minutesOfTimeStamp - minutesAnHourAgo;
+					if (m == 1) {
+						return "1 minutes ago";
+					} else {
+						return String.format("%d minutes ago", m);
+					}
 				} else {
-					return String.format("%d minutes ago", m);
+					return TODAY + localDateTime.format(timeFormatter);
 				}
+			} else if (year == now.getYear() && dayOfYear == now.getDayOfYear() - 1) {
+				return YESTERDAY + localDateTime.format(timeFormatter);
 			} else {
-				return TODAY + localDateTime.format(timeFormatter);
+				return localDateTime.format(fullDateFormatter);
 			}
-		} else if (year == now.getYear() && dayOfYear == now.getDayOfYear()-1) {
-			return YESTERDAY + localDateTime.format(timeFormatter);
-		} else {
-			return localDateTime.format(fullDateFormatter);
-		} 
+		}
+		else {
+			return "";
+		}
 	}
 	
 }
